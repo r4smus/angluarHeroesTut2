19abdefee09824246771a412372f2fa89622feaf
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-08 19:47:48 +0100

    6. Routing #6 - Dashboard Top Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
 
@@ -17,12 +18,22 @@ import { HeroService } from './hero.service';
                     {
                       path: 'heroes',
                       component: HeroesComponent
+                    },
+                    {
+                      path: 'dashboard',
+                      component: DashboardComponent
+                    },
+                    {
+                      path: '',
+                      redirectTo: '/dashboard',
+                      pathMatch: 'full'
                     }
 ])
   ],
   declarations: [ AppComponent,
                   HeroDetailComponent,
-                  HeroesComponent
+                  HeroesComponent,
+                  DashboardComponent
   ],
   providers: [HeroService],
   bootstrap:    [ AppComponent ]
