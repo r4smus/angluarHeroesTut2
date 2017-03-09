005f6f9636e3246abf517ef25f63b0f45d488543
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-09 02:12:16 +0100

    6. Routing #6 - Styling the App

--- src/app/hero-detail.component.js
+++ src/app/hero-detail.component.js
@@ -11,7 +11,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
 var common_1 = require('@angular/common');
-var hero_1 = require('./hero');
 var hero_service_1 = require('./hero.service');
 require('rxjs/add/operator/switchMap');
 var HeroDetailComponent = (function () {
@@ -30,15 +29,12 @@ var HeroDetailComponent = (function () {
     HeroDetailComponent.prototype.goBack = function () {
         this.location.back();
     };
-    __decorate([
-        core_1.Input(), 
-        __metadata('design:type', hero_1.Hero)
-    ], HeroDetailComponent.prototype, "hero", void 0);
     HeroDetailComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'my-hero-detail',
-            templateUrl: './hero-detail.component.html'
+            templateUrl: './hero-detail.component.html',
+            styleUrls: ['./hero-detail.component.css']
         }), 
         __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.ActivatedRoute, common_1.Location])
     ], HeroDetailComponent);
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -10,12 +10,12 @@ import 'rxjs/add/operator/switchMap';
 @Component({
     moduleId: module.id,
     selector: 'my-hero-detail',
-    templateUrl: './hero-detail.component.html'
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
 
-    @Input()
     hero: Hero;
 
     constructor(
